77876fdd0791bcc6a54aa600f676a171cda73da0
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2023-03-08 11:07:06 -0500

    fix "flattened2.json" offsets /=8; docs

---
+++ docs/_app/immutable/chunks/2.7377858e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.cddf4474.js";export{t as component};
--- docs/_app/immutable/chunks/2.c87701a6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.696b657c.js";export{t as component};
